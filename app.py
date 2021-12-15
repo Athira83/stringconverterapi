0579b60ffd04568f0daf8f34c4bf214fca443e9a
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-15 21:23:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,16 @@ def fizzbuzz(number):
      else:
         answer = number
      return {'answer': answer}    
-@app.route('/apiconverter')
+@app.route('/apiconverter/{text}')
 def process_string(text):
     ''' The  function takes as input a string and adds a copy right symbol
     after the names of the companies as specified in keywords '''
-    
+    from flask import request
     import re
     keywords=['deloitte','oracle','google','microsoft','amazon']                # List of words which has to be replaced
     response_text=''                                                            # Creating an empty response text
     # text=str(text)                                                              # String of input
+    text = request.args.get('text') 
     text=text.split(' ')                                                        # Splitting the string by spaces
     for word in text:                                                           # Recreating the text with new words
         if word.lower() in keywords:                                                
