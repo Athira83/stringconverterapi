d86f4578df34e711d065f15078750758694a4da8
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-15 16:54:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,19 @@ def fizzbuzz(number):
      else:
         answer = number
      return {'answer': answer}    
+@app.route('/apiconverter/{text}')
+def process_string(text):
+    ''' The  function takes as input a string and adds a copy right symbol
+    after the names of the companies as specified in keywords '''
+    
+    import re
+    keywords=['deloitte','oracle','google','microsoft','amazon']                # List of words which has to be replaced
+    response_text=''                                                            # Creating an empty response text
+    text=text.split(' ')                                                        # Splitting the string by spaces
+    for word in text:                                                           # Recreating the text with new words
+        if word.lower() in keywords:                                                
+            word=word+u"\u00a9"
+        else:
+            pass
+        response_text+=word+' '
+    return response_text
