6cc51912ff6e8aaeae6e2f80b84a4d8fecf2d621
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-15 16:44:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ from chalice import Chalice
 # import json
 app = Chalice(app_name='stringconverterapi')
 @app.route('/') 
- def index(): 
-     return {'hello': 'world'}
-# @app.route('/fizzbuzz/{number}')   
-#  def fizzbuzz(number):
-#      number = int(number)
-#      if number % 3 == 0 and number % 5 == 0:
-#         answer = "FizzBuzz    elif number % 5 == 0:
-#         answer = "Buzz"
-#      elif number % 3 == 0:
-#         answer = "Fizz"
-#      else:
-#         answer = number
-#      return {'answer': answer}
-
+def index(): 
+    return {'hello': 'world'}
+    
+@app.route('/fizzbuzz/{number}')   
+def fizzbuzz(number):
+     number = int(number)
+     if number % 3 == 0 and number % 5 == 0:
+        answer = "FizzBuzz"    
+     elif number % 5 == 0:
+        answer = "Buzz"
+     elif number % 3 == 0:
+        answer = "Fizz"
+     else:
+        answer = number
+     return {'answer': answer}    
