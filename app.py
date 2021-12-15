a286801111ce0a367aeb1e0083a0cabb6fc7ddca
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-15 16:12:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,26 +2,37 @@ from chalice import Chalice
 # import json
 app = Chalice(app_name='stringconverterapi')
 
+@app.route('/fizzbuzz/{number}')   
+ def fizzbuzz(number):
+     number = int(number)
+     if number % 3 == 0 and number % 5 == 0:
+        answer = "FizzBuzz    elif number % 5 == 0:
+        answer = "Buzz"
+     elif number % 3 == 0:
+        answer = "Fizz"
+     else:
+        answer = number
+     return {'answer': answer}
 
-@app.route('/test/text=<enter text>')
-# def index():
-#     return {'hello': 'world'}
+# @app.route('/test/text=<enter text>')
+# # def index():
+# #     return {'hello': 'world'}
 
-def process_string(text):
-    ''' The  function takes as input a string and adds a copy right symbol
-    after the names of the companies as specified in keywords '''
+# def process_string(text):
+#     ''' The  function takes as input a string and adds a copy right symbol
+#     after the names of the companies as specified in keywords '''
     
-    import re
-    keywords=['deloitte','oracle','google','microsoft','amazon']                # List of words which has to be replaced
-    response_text=''                                                            # Creating an empty response text
-    text=text.split(' ')                                                        # Splitting the string by spaces
-    for word in text:                                                           # Recreating the text with new words
-        if word.lower() in keywords:                                                
-            word=word+u"\u00a9"
-        else:
-            pass
-        response_text+=word+' '
-    return response_text
+#     import re
+#     keywords=['deloitte','oracle','google','microsoft','amazon']                # List of words which has to be replaced
+#     response_text=''                                                            # Creating an empty response text
+#     text=text.split(' ')                                                        # Splitting the string by spaces
+#     for word in text:                                                           # Recreating the text with new words
+#         if word.lower() in keywords:                                                
+#             word=word+u"\u00a9"
+#         else:
+#             pass
+#         response_text+=word+' '
+#     return response_text
     
 
 # def lambda_handler(event, context):
