63a376dec1c223498c8dd3ae8965ec60aaf3dc86
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-19 21:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,6 @@
 from chalice import Chalice
 # import json
 app = Chalice(app_name='stringconverterapi')
-@app.route('/') 
-def index(): 
-    return {'hello': 'world'}
-
-
-@app.route('/fizzbuzz/{number}')   
-def fizzbuzz(number):
-     number = int(number)
-     if number % 3 == 0 and number % 5 == 0:
-        answer = "FizzBuzz"    
-     elif number % 5 == 0:
-        answer = "Buzz"
-     elif number % 3 == 0:
-        answer = "Fizz"
-     else:
-        answer = number
-     return {'answer': answer}    
-@app.route('/apiconverter/{text}')
 def process_string(text):
     
     ''' The  function takes as input a string and adds a copy right symbol
